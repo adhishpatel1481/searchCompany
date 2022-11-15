a7890e7315490f9f78e041f19bbb0d193626f4d4
ashish <ashishpatel1481@gmail.com>
2022-11-16 00:43:34 +0530

    second commit

--- frontend/src/components/Ads.jsx
+++ frontend/src/components/Ads.jsx
@@ -1,13 +1,12 @@
 import React, { useEffect, useState } from 'react'
 import {Link} from 'react-router-dom'
 const Ads = ({data}) => {
+  const lis=data.imageUrl.split('/');
   return (
     <div className='ad'>
-        <a href={data.Advertiser[0].url}> <img className='image' src={data.imageUrl} alt="image"/></a>
+        <a href={data.Advertiser[0].url}> <img className='image' src={`http://drive.google.com/uc?export=view&id=${lis[5]}`} alt="image"/></a>
         <div className='adDetail'>
         <p>{data.Advertiser[0].name}</p>
-        <p>{data.primaryText}</p>
-        <p>{data.headline}</p>
         </div>
     </div>
   )
